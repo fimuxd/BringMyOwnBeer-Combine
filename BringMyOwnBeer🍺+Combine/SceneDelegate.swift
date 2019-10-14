ddb14df7d0457cff3b366cfc7cab9e599774ba1a
fimuxd <fimuxd@gmail.com>
2019-10-14 23:55:43 +0900

    initial commit

--- BringMyOwnBeer🍺+Combine/SceneDelegate.swift
+++ BringMyOwnBeer🍺+Combine/SceneDelegate.swift
@@ -15,20 +15,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        guard let windowScene = scene as? UIWindowScene else { return }
 
-        // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
+        let punkService = PuckServiceImpl()
+        let beerListViewModel = BeerListViewModelWithCombine(punkService: punkService)
+        let beerList = BeerList(viewModel: beerListViewModel)
 
-        // Use a UIHostingController as window root view controller.
-        if let windowScene = scene as? UIWindowScene {
-            let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView)
-            self.window = window
-            window.makeKeyAndVisible()
-        }
+        // Use a UIHostingController as window root view controller
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = UIHostingController(rootView: beerList)
+        window.makeKeyAndVisible()
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {

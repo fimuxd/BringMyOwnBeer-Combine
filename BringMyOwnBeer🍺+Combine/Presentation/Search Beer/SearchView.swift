ddfc630876d1ead3fdce1d26ec1fe544ab25ceb1
박보영 <boyoung@rainist.com>
2019-10-15 16:48:09 +0900

    add search result view

---
+++ BringMyOwnBeer🍺+Combine/Presentation/Search Beer/SearchView.swift
@@ -0,0 +1,49 @@
+//
+//  SearchView.swift
+//  BringMyOwnBeer🍺+Combine
+//
+//  Created by Bo-Young PARK on 2019/10/15.
+//  Copyright © 2019 Boyoung Park. All rights reserved.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    @ObservedObject var viewModel: SearchViewModel
+    
+    init(viewModel: SearchViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .center) {
+                SearchBar(text: $viewModel.id)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                Spacer()
+                content()
+                Spacer()
+            }
+            .navigationBarTitle("ID 검색")
+        }
+    }
+}
+
+private extension SearchView {
+    func content() -> some View {
+        if let beer = viewModel.beer {
+            return AnyView(details(for: beer))
+        } else {
+            return AnyView(loading)
+        }
+    }
+    
+    func details(for beer: Beer) -> some View {
+        SearchResultView(beer: beer)
+    }
+    
+    var loading: some View {
+        Text("\(viewModel.id)번 맥주를 불러오는 중...")
+            .foregroundColor(.gray)
+    }
+}
---
+++ BringMyOwnBeer🍺+Combine/Presentation/Search Beer/SearchViewModel.swift
@@ -0,0 +1,55 @@
+//
+//  SearchViewModel.swift
+//  BringMyOwnBeer🍺+Combine
+//
+//  Created by Bo-Young PARK on 2019/10/15.
+//  Copyright © 2019 Boyoung Park. All rights reserved.
+//
+
+import Combine
+import SwiftUI
+
+class SearchViewModel: ObservableObject {
+    @Published var beer: Beer? = nil
+    @Published var id: String = ""
+    
+    private let punkService: PunkService
+    private var disposables = Set<AnyCancellable>()
+    
+    init(
+        punkService: PunkService,
+        scheduler: DispatchQueue = DispatchQueue(label: "SearchViewModel")
+    ) {
+        self.punkService = punkService
+        _ = $id
+            .dropFirst(1)
+            .debounce(for: .seconds(0.5), scheduler: scheduler)
+            .sink(receiveValue: getBeer(id:))
+    }
+    
+    func getBeer(id: String) {
+        punkService.getBeer(id: id)
+            .print("xxx0")
+            .receive(on: DispatchQueue.main)
+            .sink(
+                receiveCompletion: { [weak self] value in
+                    guard let self = self else {
+                        return
+                    }
+                    switch value {
+                    case .failure:
+                        self.beer = nil
+                    case .finished:
+                        break
+                    }
+                },
+                receiveValue: { [weak self] beers in
+                    guard let self = self else {
+                        return
+                    }
+                    self.beer = beers.first
+                }
+            )
+            .store(in: &disposables)
+    }
+}
